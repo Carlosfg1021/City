004eccd61b84a4f8bc572c026d58dc4391bf9c11
Carlos Daniel Franco Pérez <carlosfg1021@gmail.com>
2020-10-12 11:40:06 -0600

    Initial commit

---
+++ app/src/main/java/com/example/city/Inicio/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.city.Inicio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.city.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/city/PortadaActivity.java
@@ -0,0 +1,27 @@
+package com.example.city;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.city.Inicio.LoginActivity;
+
+public class PortadaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(PortadaActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        }, 2000);
+    }
+}
