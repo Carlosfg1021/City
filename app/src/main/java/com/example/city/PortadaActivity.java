5fbd97eb311069bf9eb1fc51670a3ce5e2dc68c7
AJMG3011 <62410887+ajmg3011@users.noreply.github.com>
2020-10-16 19:53:02 -0600

    Inicio de Sesion con google y guardado de datos en Firebase

--- app/src/main/java/com/example/city/PortadaActivity.java
+++ app/src/main/java/com/example/city/PortadaActivity.java
@@ -6,7 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import com.example.city.Inicio.LoginActivity;
+import com.example.city.Inicio.AuthActivity;
 import com.example.city.Modelos.MainActivity;
 
 public class PortadaActivity extends AppCompatActivity {
@@ -20,7 +20,7 @@ public class PortadaActivity extends AppCompatActivity {
             @Override
             public void run() {
 
-                Intent intent = new Intent(PortadaActivity.this, MainActivity.class);
+                Intent intent = new Intent(PortadaActivity.this, AuthActivity.class);
                 startActivity(intent);
             }
         }, 2000);
