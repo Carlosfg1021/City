7f0bc30d5ec871ba5ecf3b4da3fb871fbc1ef25f
Carlos Daniel Franco Pérez <carlosfg1021@gmail.com>
2020-10-27 12:26:08 -0600

    Cargar datos cardview

--- app/src/main/java/com/example/city/Adaptadores/AdaptadorUsuarios.java
+++ app/src/main/java/com/example/city/Adaptadores/AdaptadorUsuarios.java
@@ -8,91 +8,51 @@ import android.widget.TextView;
 
 import com.example.city.Entidades.Usuarios;
 import com.example.city.R;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 
 import java.util.ArrayList;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class AdaptadorUsuarios extends RecyclerView.Adapter<AdaptadorUsuarios.ViewHolder> implements  View.OnClickListener {
-
-
-    LayoutInflater inflater;
-    ArrayList<Usuarios>model;
-    private View.OnClickListener listener;
-
-
-    public AdaptadorUsuarios(Context context, ArrayList<Usuarios> model){
-
-
-        this.inflater = LayoutInflater.from(context);
-        this.model = model;
-
+public class AdaptadorUsuarios extends FirebaseRecyclerAdapter<Usuarios,AdaptadorUsuarios.AdaptadorUs> {
+    /**
+     * Initialize a {@link RecyclerView.Adapter} that listens to a Firebase query. See
+     * {@link FirebaseRecyclerOptions} for configuration options.
+     *
+     * @param options
+     */
+    public AdaptadorUsuarios(@NonNull FirebaseRecyclerOptions<Usuarios> options) {
+        super(options);
     }
 
+    @Override
+    protected void onBindViewHolder(@NonNull AdaptadorUs holder, int position, @NonNull Usuarios usuarios) {
+
+        holder.txtnickname.setText(usuarios.getNickname());
+        holder.txtciudad.setText(usuarios.getInstitucion());
+
+    }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = inflater.inflate(R.layout.cardview_usuarios_ciudad,parent,false);
-        view.setOnClickListener(this);
-        return new ViewHolder(view);
+    public AdaptadorUs onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_usuarios_ciudad,parent,false);
+        return new AdaptadorUs(view);
     }
 
+    // public  TextView txtnickname,txtciudad;
 
-    public void setOnClickListener(View.OnClickListener listener){
+   public class AdaptadorUs extends RecyclerView.ViewHolder{
 
-        this.listener = listener;
-    }
+       TextView txtnickname,txtciudad;
 
+       public AdaptadorUs(@NonNull View itemView) {
+           super(itemView);
+           txtnickname = itemView.findViewById(R.id.txtnickname);
+           txtciudad = itemView.findViewById(R.id.txtciudad);
 
-    @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
-
-      String nickname = model.get(position).getNickname();
-      String ciudad = model.get(position).getNombre_ciudad();
-
-      holder.nickname.setText(nickname);
-      holder.ciudad.setText(ciudad);
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return model.size();
-    }
-
-
-    @Override
-    public void onClick(View view) {
-
-        if(listener!=null){
-
-            listener.onClick(view);
-
-        }
-
-    }
-
-
-    public class ViewHolder extends RecyclerView.ViewHolder{
-
-       TextView nickname, ciudad;
-        public ViewHolder(@NonNull View itemView){
-            super(itemView);
-
-
-            nickname = itemView.findViewById(R.id.txtnickname);
-            ciudad = itemView.findViewById(R.id.txtciudad);
-
-        }
-
-
-    }
-
-
-
-
-
+       }
+   }
 }
